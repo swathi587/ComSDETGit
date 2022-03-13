b1b1e8fb5aea84cdbf38db69e9a6da6ef0867557
dell <dell@192.168.0.132>
2022-03-13 23:13:37 +0530

    New Commit

---
+++ src/test/java/demoGittt/Samgit.java
@@ -0,0 +1,13 @@
+package demoGittt;
+
+import org.testng.annotations.Test;
+
+
+public class Samgit {
+	@Test
+	public void demo()
+	{
+		System.out.println("First Committt");
+	System.out.println("second Committt");
+}
+}
