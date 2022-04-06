5132629ee413cf67b09ab36dd71f113f1ef10965
madhu-madhu123 <hbmadhu13@gmail.com>
2022-04-06 14:48:15 +0530

    commit branch push

--- src/test/java/demoGittt/Samgit.java
+++ src/test/java/demoGittt/Samgit.java
@@ -12,5 +12,7 @@ public class Samgit {
 	System.out.println("second Committt");
 	System.out.println("Third commit");
 	System.out.println("Third commit");
+	
+	System.out.println("hi swathi");
 }
 }
